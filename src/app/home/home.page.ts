025f386bf8b89f7499786ddf6993fad0600a7e22
Alejandro <alexsotog82@gmail.com>
2020-04-30 10:56:37 -0500

    se sube fron de multer

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,3 +1,4 @@
+import { UploadService } from './../services/upload.service';
 import { Component } from '@angular/core';
 
 @Component({
@@ -7,6 +8,24 @@ import { Component } from '@angular/core';
 })
 export class HomePage {
 
-  constructor() {}
+  photoSelected: string | ArrayBuffer;
+  file: File;
+
+  constructor(private _uploadService: UploadService) {}
+
+
+  uploadPhoto(title: HTMLInputElement, description: HTMLInputElement) {
+    this._uploadService.uploadImage(title.value, description.value, this.file).subscribe(data => console.log(data));
+  }
+
+  onPhotoSelected(event): void {
+    if (event.target.files && event.target.files[0]) {
+      this.file = <File>event.target.files[0];
+      // image preview
+      const reader = new FileReader();
+      reader.onload = e => this.photoSelected = reader.result;
+      reader.readAsDataURL(this.file);
+    }
+  }
 
 }
